81aed1652d374339ee95d307907b981bceb9a3c5
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2020-04-18 09:47:13 -0700

    Revert "serviceExists ShirtService test passing"
    This reverts commit cd3cc32773ea161ee34ea2663ed8174cb0e693dd.

--- src/main/java/com/cottonfactory/products/services/ShirtService.java
+++ src/main/java/com/cottonfactory/products/services/ShirtService.java
@@ -1,13 +1,7 @@
 package com.cottonfactory.products.services;
 
-import com.cottonfactory.products.repositories.ShirtRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ShirtService {
-    private ShirtRepository shirtRepository;
-
-    public ShirtService(ShirtRepository shirtRepository) {
-        this.shirtRepository = shirtRepository;
-    }
 }
