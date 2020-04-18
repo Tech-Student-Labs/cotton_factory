fb3c13e671e15542fbfcb856a711da65078ed6de
jimmy-bellinghausen <jimmy.bellinghausen@gmail.com>
2020-04-18 13:59:45 -0500

    postShirt service test written

--- src/test/java/com/cottonfactory/products/services/ShirtServiceTest.java
+++ src/test/java/com/cottonfactory/products/services/ShirtServiceTest.java
@@ -1,11 +1,16 @@
 package com.cottonfactory.products.services;
 
+import com.cottonfactory.products.entities.Shirt;
 import com.cottonfactory.products.repositories.ShirtRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class ShirtServiceTest {
@@ -21,5 +26,15 @@ public class ShirtServiceTest {
         //Assert
         assertNotNull(shirtService);
         //Teardown
+    }
+
+    @Test
+    void postShirt(){
+        Shirt expected = new Shirt(Shirt.TYPE.TEE, 3, 3, "M", "customSize", "Red", true, BigDecimal.valueOf(30.5));
+        Shirt input = new Shirt();
+        input.setType(expected.getType());
+        expected.setId(1L);
+        when(shirtRepository.save(any(Shirt.class))).thenReturn(expected);
+        assertEquals(expected, service.postShirt(input));
     }
 }
