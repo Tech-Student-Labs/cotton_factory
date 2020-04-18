cd3cc32773ea161ee34ea2663ed8174cb0e693dd
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2020-04-18 09:45:16 -0700

    serviceExists ShirtService test passing

---
+++ src/main/java/com/cottonfactory/products/entities/Shirt.java
@@ -0,0 +1,13 @@
+package com.cottonfactory.products.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Shirt {
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private Long id;
+}
